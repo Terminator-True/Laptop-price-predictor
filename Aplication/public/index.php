597eea14355b1b6e933bbf9f7dd21d671b002bdf
Terminator-True <jf28renault@gmail.com>
2024-05-02 19:41:24 +0200

    ADD: gitignore

--- Aplication/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '..\\config\\Kernel.php';
-$init = new app\config\Kernel();
-
-// include_once APPROOT.'app/Views/layout/app.php';
-
-
-?>
--- machine_learning/scripts/test.py
+++ machine_learning/scripts/test.py
@@ -1,10 +1,12 @@
 import pickle
 import numpy as np
+import os
 
-pipe = pickle.load(open('models\\pipe_main.pkl','rb'))
+# print(os.getcwd())
+pipe = pickle.load(open('machine_learning\\models\\pipe_main.pkl','rb'))
 # pipe = pickle.load(open('pipe.pkl','rb')) #test Model
 
-query = np.array(['Apple',15.6,'intel core i7',32,'Nvidia High gamma','MacOs',2000], dtype=object)
+query = np.array(['Lenovo',15.6,'intel core i7',32,'Nvidia High gamma','Windows',2000], dtype=object)
 # query = np.array(['Lenovo','Gaming',16,32,'Windows',2.50,1,0,141,'Intel Core i7',256,1000,'Nvidia'], dtype=object) # Test Model
 query = query.reshape(1,7)
 
